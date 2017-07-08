ea7ef763f89c9e8a1c37f953643fd07d46580dd7
chaithu <godavarthi.chaithu@gmail.com>
2017-07-08 16:33:34 +0530

    modified the method types from get to post

--- build/resources/main/data-h2.sql
+++ build/resources/main/data-h2.sql
@@ -1,11 +1,11 @@
-INSERT INTO User_Master(UserId,EmailAddress, ContactNumber, FirstName,MiddleName,LastName,UserType,LoginPassword,CreatedOn,PasswordChangedOn)
-VALUES ('RM1001','godavarthi.chaithu@gmail.com','8019066838','Godavarthi','G','VSRK','Normal','chaitanya','2017-07-01','2017-07-01');
+INSERT INTO User_Master(userid,emailaddress, contactnumber, firstname,middlename,lastname,usertype,loginpassword,createdon,passwordchangedon)
+VALUES ('RM1001','RM1001@gmail.com','8019066838','RM1001','RM1','RM1','Normal','M0h7GHp287CWNRBYMdC08w==','2017-07-01','2017-07-01');
 
+INSERT INTO User_Master(userid,emailaddress, contactnumber, firstname,middlename,lastname,usertype,loginpassword,createdon,passwordchangedon)
+VALUES ('RM1002','RM1002@gmail.com','8019066838','RM1002','RM2','RM2','Normal','tsm9Oicxd1MzPclArQmyuA==','2017-07-01','2017-07-01');
 
+INSERT INTO User_Master(userid,emailaddress, contactnumber, firstname,middlename,lastname,usertype,loginpassword,createdon,passwordchangedon)
+VALUES ('RM1003','RM1003@gmail.com','8019066838','RM1003','RM3','RM3','Normal','0RowwHPmQIytprUBzTUZiw==','2017-07-01','2017-07-01');
 
-
-
-
-
-
-
+INSERT INTO User_Master(userid,emailaddress, contactnumber, firstname,middlename,lastname,usertype,loginpassword,createdon,passwordchangedon)
+VALUES ('RM1004','RM1004@gmail.com','8019066838','RM1004','RM4','RM4','Normal','bhKrW4FDRgIeQ9DmWJUB/A==','2017-07-01','2017-07-01');
--- build/resources/main/schema-h2.sql
+++ build/resources/main/schema-h2.sql
@@ -1,14 +1,16 @@
 CREATE TABLE User_Master
-( UserId  varchar2(50),
-  EmailAddress varchar2(50),
-  ContactNumber number(20),
-  FirstName varchar2(50),
-  MiddleName varchar2(50),
-  LastName varchar2(50),
-  UserType varchar2(10),
-  LoginPassword varchar2(50),
-  CreatedOn DATE,
-  PasswordChangedOn DATE,
+( userid  varchar2(50),
+  emailaddress varchar2(50),
+  contactnumber varchar2(50),
+  firstname varchar2(50),
+  middlename varchar2(50),
+  lastname varchar2(50),
+  usertype varchar2(10),
+  loginpassword varchar2(50),
+  createdon varchar2(10),
+  passwordchangedOn varchar2(10),  
   CONSTRAINT customers_pk PRIMARY KEY (UserId),
   CONSTRAINT USER_EmailAddress UNIQUE(EmailAddress,ContactNumber)
 );
+
+
