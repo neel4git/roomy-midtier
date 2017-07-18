f7ff543bb51ad17d3c00500351195edb7b074159
chaitanya <godavarthi.chaithu@gmail.com>
2017-07-19 00:01:06 +0530

    azur testing with new branch

--- src/main/java/com/Roomy/Application.java
+++ src/main/java/com/Roomy/Application.java
@@ -5,6 +5,7 @@ import java.text.ParseException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -14,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Configuration
 @PropertySource("classpath:/application.properties")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) throws SQLException, ParseException {
 		SpringApplication.run(Application.class, args);
 	}
