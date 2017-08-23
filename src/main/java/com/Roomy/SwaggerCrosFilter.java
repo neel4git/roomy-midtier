cd1c29ef14d21114beb0e82864870f5cc32be8bd
chaitanya <godavarthi.chaithu@gmail.com>
2017-08-23 06:41:03 +0530

    restest

--- src/main/java/com/Roomy/SwaggerCrosFilter.java
+++ src/main/java/com/Roomy/SwaggerCrosFilter.java
@@ -15,27 +15,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class SwaggerCrosFilter implements Filter {
 
-	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletResponse reponse = (HttpServletResponse) res;
-		reponse.setHeader("Acess-Control-Allow-Origin", "*");
-		reponse.setHeader("Acess-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-		reponse.setHeader("Acess-Control-Max-Age", "3600");
-		reponse.setHeader("Acess-Control-Allow-Headers", "x-requested-with");
-		reponse.setHeader("Cache-Control", "no-cache,no-store,must-revalidate");
-		reponse.setHeader("Pragma", "np-cache");
-		reponse.setHeader("Expires", "0");
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Expires", "0");
 		chain.doFilter(req, res);
 	}
 
-	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		// Do Nothing
 
 	}
 
-	@Override
 	public void destroy() {
 		// Do Nothing
 
