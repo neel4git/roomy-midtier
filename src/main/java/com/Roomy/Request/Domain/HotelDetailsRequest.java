7baaade8b9cf7b24a9b20d272bb5984eeb87944a
bethapudi <santhosh.bethapudi@gmail.com>
2017-08-27 22:58:09 +0530

    HotelDtails Request

---
+++ src/main/java/com/Roomy/Request/Domain/HotelDetailsRequest.java
@@ -0,0 +1,21 @@
+package com.Roomy.Request.Domain;
+
+public class HotelDetailsRequest {
+	
+	private int hotelId;
+	private String token;
+	public int getHotelId() {
+		return hotelId;
+	}
+	public void setHotelId(int hotelId) {
+		this.hotelId = hotelId;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+
+}
